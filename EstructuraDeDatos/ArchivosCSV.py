a0263e8d5eb9d9f517baed0876bda02a851072ce
NicolasGustavoFranco <142341202+NicolasGustavoFranco@users.noreply.github.com>
2023-09-11 17:07:42 -0500

    Update ArchivosCSV.py

--- EstructuraDeDatos/ArchivosCSV.py
+++ EstructuraDeDatos/ArchivosCSV.py
@@ -1,11 +1,97 @@
+# importing csv module
 import csv
-with open('eggs.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
+ 
+# csv file name
+filename = "aapl.csv"
+ 
+# initializing the titles and rows list
+fields = []
+rows = []
+ 
+# reading csv file
+with open(filename, 'r') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
+     
+    # extracting field names through first row
+    fields = next(csvreader)
+ 
+    # extracting each data row one by one
+    for row in csvreader:
+        rows.append(row)
+ 
+    # get total number of rows
+    print("Total no. of rows: %d"%(csvreader.line_num))
+ 
+# printing the field names
+print('Field names are:' + ', '.join(field for field in fields))
+ 
+# printing first 5 rows
+print('\nFirst 5 rows are:\n')
+for row in rows[:5]:
+    # parsing each column of a row
+    for col in row:
+        print("%10s"%col,end=" "),
+    print('\n')
 
-  with open('eggs.csv', 'w', newline='') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=' ',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
-    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+
+# Writing csv
+# field names
+fields = ['Name', 'Branch', 'Year', 'CGPA']
+ 
+# data rows of csv file
+rows = [ ['Nikhil', 'COE', '2', '9.0'],
+        ['Sanchit', 'COE', '2', '9.1'],
+        ['Aditya', 'IT', '2', '9.3'],
+        ['Sagar', 'SE', '1', '9.5'],
+        ['Prateek', 'MCE', '3', '7.8'],
+        ['Sahil', 'EP', '2', '9.1']]
+ 
+# name of csv file
+filename = "university_records.csv"
+ 
+# writing to csv file
+with open(filename, 'w') as csvfile:
+    # creating a csv writer object
+    csvwriter = csv.writer(csvfile)
+     
+    # writing the fields
+    csvwriter.writerow(fields)
+     
+    # writing the data rows
+    csvwriter.writerows(rows)
+
+
+#Diccionario
+import csv
+ 
+# my data rows as dictionary objects
+mydict =[{'branch': 'COE', 'cgpa': '9.0',
+          'name': 'Nikhil', 'year': '2'},
+        {'branch': 'COE', 'cgpa': '9.1',
+         'name': 'Sanchit', 'year': '2'},
+        {'branch': 'IT', 'cgpa': '9.3',
+         'name': 'Aditya', 'year': '2'},
+        {'branch': 'SE', 'cgpa': '9.5',
+         'name': 'Sagar', 'year': '1'},
+        {'branch': 'MCE', 'cgpa': '7.8',
+         'name': 'Prateek', 'year': '3'},
+        {'branch': 'EP', 'cgpa': '9.1',
+         'name': 'Sahil', 'year': '2'}]
+ 
+# field names
+fields = ['name', 'branch', 'year', 'cgpa']
+ 
+# name of csv file
+filename = "university_records.csv"
+ 
+# writing to csv file
+with open(filename, 'w') as csvfile:
+    # creating a csv dict writer object
+    writer = csv.DictWriter(csvfile, fieldnames = fields)
+     
+    # writing headers (field names)
+    writer.writeheader()
+     
+    # writing data rows
+    writer.writerows(mydict)
